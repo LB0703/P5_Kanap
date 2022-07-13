451c06a46afca97f92a503f2453f83965cf36ee5
laetitia <abs.amel@free.fr>
2022-07-13 19:48:50 +0200

    add files js

---
+++ front/js/product.js
@@ -0,0 +1,18 @@
+console.log ('olp');
+
+// <img src="http://localhost:3000/images/kanap01.jpeg" alt="Photo d'un canapé bleu, deux places"> */
+
+
+<h1 id="Kanap Sinopé"></h1> // Nom du produit
+
+// 42
+<p>Prix : 1849 <span id="price"></span>€</p>
+
+//Dis enim malesuada risus sapien gravida nulla nisl arcu. 
+<p id="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+/*<option value="blue">bleu</option>
+      <option value="white">blanc</option>
+      <option value="black">noir</option>
+      */
